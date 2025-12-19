6880edb92f34f00eddc3f0b53770409eddd5ff9d
Zaggy1024 <Zaggy1024@gmail.com>
2025-12-19 15:13:42 -0600

    Meta: Treat MediaSource as a platform object
    This allows URL.createObjectURL(new MediaSource()) to succeed.

--- Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/IDLGenerators.cpp
+++ Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/IDLGenerators.cpp
@@ -96,6 +96,7 @@ static bool is_platform_object(Type const& type)
         "KeyframeEffect"sv,
         "MediaKeySystemAccess"sv,
         "MediaList"sv,
+        "MediaSource"sv,
         "Memory"sv,
         "MessagePort"sv,
         "Module"sv,
