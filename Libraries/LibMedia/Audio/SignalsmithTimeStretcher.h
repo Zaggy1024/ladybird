18713ec8ccfe8cf38007d04efee7bcaa080378f7
Zaggy1024 <Zaggy1024@gmail.com>
2026-04-30 23:43:14 -0500

    signalsmith 1

---
+++ Libraries/LibMedia/Audio/SignalsmithTimeStretcher.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026-present, the Ladybird developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Error.h>
+#include <AK/NonnullOwnPtr.h>
+#include <LibMedia/Audio/TimeStretcher.h>
+
+namespace Audio {
+
+class SignalsmithTimeStretcher final : public TimeStretcher {
+public:
+    static ErrorOr<NonnullOwnPtr<TimeStretcher>> create(u32 sample_rate, u8 channel_count);
+    virtual ~SignalsmithTimeStretcher() override;
+
+    virtual i64 flush_and_get_preroll(AK::Duration media_start_timestamp, i64 output_start_frame_index) override;
+    virtual void set_rate(float rate) override;
+    virtual void set_pitch(float pitch) override;
+    virtual void push_block(Media::AudioBlock const& input) override;
+    virtual Media::DecoderErrorOr<Media::AudioBlock> retrieve_block() override;
+    virtual void signal_end_of_stream() override;
+
+private:
+    struct Impl;
+
+    explicit SignalsmithTimeStretcher(NonnullOwnPtr<Impl>);
+    NonnullOwnPtr<Impl> m_impl;
+};
+
+}
